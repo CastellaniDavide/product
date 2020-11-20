b06d9b4ea3981180abc563ff0172bb72d1c63970
Davide Castellani <38205382+DavideC03@users.noreply.github.com>
2020-11-20 09:01:17 +0100

    Created bin/template.py

---
+++ bin/product.py
@@ -0,0 +1,19 @@
+"""product
+"""
+
+__author__ = "help@castellanidavide.it"
+__version__ = "1.0 2020-11-20"
+
+class product:
+	def __init__ (self):
+		"""Where it all begins
+		"""
+		print(product.product())
+	
+	def product():
+		"""product first funtion
+		"""
+		return "product" #for the test (see test_product.py)
+		
+if __name__ == "__main__":
+	product()
